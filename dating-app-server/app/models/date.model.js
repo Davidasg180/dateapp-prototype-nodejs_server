1c3e3c285227d3f40325b4fe31f32b40fbd98dba
David Alberto Serrano Garcia <Davidasg180@gmail.com>
2018-06-29 01:22:21 -0700

    Update monoogse date model Add user relationships

--- dating-app-server/app/models/date.model.js
+++ dating-app-server/app/models/date.model.js
@@ -1,6 +1,9 @@
 const { Schema, Model } = require(`mongoose`);
 
 let dateSchema = Schema({
+    _userOne: Schema.Types.ObjectId,
+    _userTwo: Schema.Types.ObjectId,
+    
     _place: Schema.Types.ObjectId
 });
 
