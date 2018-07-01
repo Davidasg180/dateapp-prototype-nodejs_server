9798db36e2d2be75ef541a08e2628d521dd8c2de
David Alberto Serrano Garcia <Davidasg180@gmail.com>
2018-07-01 10:31:35 -0700

    Add: Include react-router-dom

--- dating-app-web-panel/src/App.js
+++ dating-app-web-panel/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import {
+    Link,
+    Route,
+    Switch,
+    BrowserRouter as Router
+} from 'react-router-dom'
+
+import MapView from './views/MapView';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <ul>
+                        <li><Link to="/map">Map</Link></li>
+                    </ul>
+                    <Switch>
+                        <Route
+                            path="/map"
+                            component="MapView"
+                        />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ dating-app-web-panel/src/views/MapView.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+class MapView extends Comment {
+    render() {
+        return (
+            <div>
+                <h1>Hello im MapView</h1>
+            </div>
+        );
+    }
+}
+
+export default MapView;
