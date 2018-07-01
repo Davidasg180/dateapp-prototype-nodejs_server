f61f9b4e08ba72d261a8d37e197fdcb46ab75ae4
David Alberto Serrano Garcia <Davidasg180@gmail.com>
2018-07-01 01:35:50 -0700

    Add : Material UI package and implement sidebar Drawer Add: package lock

--- dating-app-web-panel/src/App.js
+++ dating-app-web-panel/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import {
+    Divider,
+    Drawer,
+    ListItem,
+    ListItemIcon,
+    ListItemText
+} from '@material-ui/core';
 import './App.css';
+import SideBar from './components/SideBar';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <SideBar />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ dating-app-web-panel/src/components/SideBar.js
@@ -0,0 +1,33 @@
+﻿import React ,{ Component } from "react";
+import {
+    ListItem,
+    ListItemText,
+    Drawer
+} from "@material-ui/core";
+
+
+class SideBar extends Component {
+    render() {
+        return (
+            <Drawer
+                variant="permanent"
+                anchor="left"
+            >
+                <ListItem button>
+                    <ListItemText primary="Inbox" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary="Starred" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary="Send mail" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary="Drafts" />
+                </ListItem>
+            </Drawer>
+        );
+    }
+}
+
+export default SideBar;
