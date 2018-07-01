c536032992564ef2e545228ad41baab36aa514f0
David Alberto Serrano Garcia <Davidasg180@gmail.com>
2018-06-30 23:24:44 -0700

    Add : public files react app this is render on root route.

--- dating-app-server/app/app.js
+++ dating-app-server/app/app.js
@@ -1,21 +1,33 @@
 require('./config');
 require('./helpers');
 
+const path = require(`path`);
+
 const express = require('express');
 const mongoose = require('mongoose');
 const express_graph = require('express-graphql');
 const { buildSchema, graphqlHTTP } = require('graphql');
+
 const middleware = require('./middleware');
 
 const models = require('./models');
 const schema = require('./graphql');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(middleware);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 let rootValue = {
     message: () => middleware
--- dating-app-server/app/routes/index.js
+++ dating-app-server/app/routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // if (!req.user.admin) return res.sendStatus(401);
-    res.sendStatus(200);
-    res.send("HUrRay!!");
+    //res.sendStatus(200);
+    //res.send("HUrRay!!");
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
