042100dced9e38f6d06954fd0b8b32865a99438b
David Alberto Serrano Garcia <Davidasg180@gmail.com>
2018-07-07 21:31:16 -0700

    ADD: AccessPanel and Login for for authentication.

--- dating-app-web-panel/src/pages/LoginPage/LoginPage.js
+++ dating-app-web-panel/src/pages/LoginPage/LoginPage.js
@@ -1,36 +1,65 @@
 import React, { Component } from 'react';
+
+import './LoginPage.css'
+
 import {
     TextField,
-    Button
+    Button,
+    Grid,
+    Paper
 } from '@material-ui/core';
+
+import AccessPanel from '../../layouts/AccessPanel/AccessPanel';
+
+import Card from "./../../components/Card/Card";
+import CardHeader from "./../../components/Card/CardHeader/CardHeader";
+import CardBody from '../../components/Card/CardBody/CardBody';
 
 class LoginPage extends Component {
     state = {}
     render() {
         return (
-            <div>
-                <TextField
-                    hintText="Username"
-                    floatingLanelText="Username"
-                    onChange={(event, newValue) => {
-                        this.setState({ username: newValue })
-                    }}
-                />
-                <TextField
-                    hintText="Username"
-                    floatingLanelText="Username"
-                    onChange={(event, newValue) => {
-                        this.setState({ username: newValue });
-                    }}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-  
-                >
-                Login
-                </Button>
-            </div>
+            <AccessPanel>
+                <div className="LoginPage">
+                    <Grid container
+                        spacing={0}
+                        justify={`center`}
+                    >
+                        <Grid item xs={4}>
+                            <form noValidate autoComplete="off">
+                                <Card>
+                                    <CardHeader color="primary">
+                                        <h3 style={{ textAlign: 'center', margin: '0' }}>Login</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <TextField
+                                            fullWidth
+                                            id="email"
+                                            label="Email"
+                                            margin="normal"
+                                            InputLabelProps={{
+                                                shrink: true,
+                                            }}
+                                        />
+                                        <TextField
+                                            fullWidth
+                                            id="password"
+                                            label="Password"
+                                            margin="normal"
+                                            InputLabelProps={{
+                                                shrink: true,
+                                            }}
+                                        />
+                                        <Button fullWidth color="prmary">
+                                            LOGIN
+                                        </Button>
+                                    </CardBody>
+                                </Card>
+                            </form>
+                        </Grid>
+                    </Grid>
+                </div>
+            </AccessPanel >
         );
     }
 }
